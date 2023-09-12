3e7835ab03f4b21f77a4a6e816cfb482cb6f46c0
Kenneth Fernandes <57480946+Kenneth-Fernandes-0620@users.noreply.github.com>
2023-09-12 22:29:10 +0530

    initial commit

---
+++ public/index.js
@@ -0,0 +1,122 @@
+import * as THREE from "/node_modules/three/build/three.module.js";
+import { EffectComposer } from "/node_modules/three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "/node_modules/three/examples/jsm/postprocessing/RenderPass.js";
+import { UnrealBloomPass } from "/node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js";
+import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls.js';
+
+//global declaration
+let scene;
+let camera;
+let renderer;
+const canvas = document.getElementsByTagName("canvas")[0];
+scene = new THREE.Scene();
+const fov = 60;
+const aspect = window.innerWidth / window.innerHeight;
+const near = 0.1;
+const far = 1000;
+
+//camera
+camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 8;
+camera.position.y = 0;
+camera.position.x = 30;
+scene.add(camera);
+
+
+//default renderer
+renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.autoClear = false;
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
+renderer.setClearColor(0x000000, 0.0);
+
+//controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+//bloom renderer
+const renderScene = new RenderPass(scene, camera);
+const bloomPass = new UnrealBloomPass(
+  new THREE.Vector2(window.innerWidth, window.innerHeight),
+  1.5,
+  0.4,
+  0.85
+);
+bloomPass.threshold = 0;
+bloomPass.strength = 1; //intensity of glow
+bloomPass.radius = 0;
+const bloomComposer = new EffectComposer(renderer);
+bloomComposer.setSize(window.innerWidth, window.innerHeight);
+bloomComposer.renderToScreen = true;
+bloomComposer.addPass(renderScene);
+bloomComposer.addPass(bloomPass);
+
+//planet object
+const color = new THREE.Color("#00AAFF");
+const geometry = new THREE.SphereGeometry(10, 64, 32, 0, Math.PI * 2, 0, Math.PI);;
+
+
+// const material = new THREE.MeshBasicMaterial({ color: color });
+
+const texture = new THREE.TextureLoader().load('./texture/planet.jpg');
+texture.anisotropy = 8;
+texture.colorSpace = THREE.SRGBColorSpace;
+// texture.wrapS = THREE.RepeatWrapping;
+// texture.wrapT = THREE.RepeatWrapping;
+// texture.repeat.set(4, 4);
+
+
+
+const material = new THREE.MeshBasicMaterial({
+  map: texture,
+  overdraw: true
+});
+
+
+const sphere = new THREE.Mesh(geometry, material);
+sphere.position.set(0, 0, 0);
+sphere.layers.set(1);
+scene.add(sphere);
+
+// galaxy geometry
+const starGeometry = new THREE.SphereGeometry(80, 64, 64);
+
+// galaxy material
+const starMaterial = new THREE.MeshBasicMaterial({
+  map: THREE.ImageUtils.loadTexture("texture/galaxy.png"),
+  side: THREE.BackSide,
+  transparent: true,
+});
+
+// galaxy mesh
+const starMesh = new THREE.Mesh(starGeometry, starMaterial);
+starMesh.layers.set(1);
+scene.add(starMesh);
+
+//ambient light
+const ambientlight = new THREE.AmbientLight(0xffffff, 0.1);
+scene.add(ambientlight);
+
+//resize listner
+window.addEventListener(
+  "resize",
+  () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    bloomComposer.setSize(window.innerWidth, window.innerHeight);
+  },
+  false
+);
+
+//animation loop
+const animate = () => {
+  requestAnimationFrame(animate);
+  s
+  camera.layers.set(1);
+  bloomComposer.render();
+};
+
+animate();
